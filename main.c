9765c5ca301eabcee5c9c91b64301dd02fbc6a5a
Yassein Eltayeb Mohamed Idris <ymohamed@lab2r3s6.42abudhabi.ae>
2023-05-23 19:18:12 +0400

    up

--- main.c
+++ main.c
@@ -26,9 +26,11 @@ int	main(int argc, char **argv)
 		return (fprintf(stderr, "USAGE: monty file\n"), EXIT_FAILURE);
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
-		return (fprintf(stderr, "Error: Can't open file %s\n", argv[1]), EXIT_FAILURE);
+		return (fprintf(stderr, "Error: Can't open file %s\n",
+				argv[1]), EXIT_FAILURE);
 	err_line = do_instructions_on_file(fd);
 	if (err_line)
-		return (fprintf(stderr, "L%d: unknown instruction <opcode>\n", err_line), EXIT_FAILURE);
+		return (fprintf(stderr, "L%d: unknown instruction <opcode>\n",
+				err_line), EXIT_FAILURE);
 	return (err_line);
 }
