339d81943a8b5ad63ba2681d92d0a5235b737a57
Yassein Eltayeb Mohamed Idris <ymohamed@lab2r3s6.42abudhabi.ae>
2023-05-25 02:04:59 +0400

    up

--- main.c
+++ main.c
@@ -61,7 +61,7 @@ void	execute_line(char *line, int line_no, char *onboard,
 		f(stack, 0);
 	}
 	else
-		printf("welcome to nowhere\n");
+		unknown_err(line, line_no, onboard, fd, stack);
 }
 
 /**
@@ -100,11 +100,16 @@ int	main(int argc, char **argv)
 	int	fd;
 
 	if (argc != 2)
-		return (fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE), 1);
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
-		return (fprintf(stderr, "Error: Can't open file %s\n",
-				argv[1]), exit(EXIT_FAILURE), 1);
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 	do_instructions_on_file(fd);
 	close(fd);
 	return (0);
