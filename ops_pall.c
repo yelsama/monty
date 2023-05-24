5d019df9b3a49fe6c8a4c1c1e172d2cf9f31e1bb
Yassein Eltayeb Mohamed Idris <ymohamed@lab2r3s6.42abudhabi.ae>
2023-05-24 22:55:14 +0400

    up

--- ops_pall.c
+++ ops_pall.c
@@ -3,6 +3,7 @@
 /**
  * pall - check the code
  * @stack: head of double linked list
+ * @number: to follow the shape
  * Return: number of nodes
  */
 void	pall(stack_t **stack, unsigned int number)
