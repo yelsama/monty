2adbff474881c72dba7337c69caab7ea3a300107
Yassein Eltayeb Mohamed Idris <ymohamed@lab2r3s6.42abudhabi.ae>
2023-05-25 00:54:20 +0400

    up

--- monty.h
+++ monty.h
@@ -56,6 +56,8 @@ void	push_err(char *line, int line_no, char *onboard,
 			int fd, stack_t **stack);
 void	unknown_err(char *line, int line_no, char *onboard,
 			int fd, stack_t **stack);
+void	pint_err(char *line, int line_no, char *onboard,
+			int fd, stack_t **stack);
 
 void	pall(stack_t **stack, unsigned int number);
 void	push(stack_t **stack, unsigned int number);
