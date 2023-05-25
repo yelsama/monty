5e53c5c9a01371ade120ff822c060553fcecaebd
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-05-25 18:31:42 +0400

    up

--- main.c
+++ main.c
@@ -30,8 +30,6 @@ void	execute_line(char *line, int line_no, char *onboard,
 		tmp += 4;
 		while (*tmp == ' ')
 			tmp++;
-		if (*tmp == '+')
-			tmp++;
 		if (*tmp == '-' && (!tmp[1] || tmp[1] < '0' || tmp[1] > '9'))
 			push_err(line, line_no, onboard, fd, stack);
 		else if (!*tmp || *tmp < '0' || *tmp > '9')
