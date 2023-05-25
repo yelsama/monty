e17ae4c85ee538928bfa0920feb317e5293efe12
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-05-25 17:56:03 +0400

    up

--- main.c
+++ main.c
@@ -34,7 +34,7 @@ void	execute_line(char *line, int line_no, char *onboard,
 	if (f == pall)
 		if (tmp[4] != 0 && tmp[4] != ' ' && tmp[4] != '\n')
 			unknown_err(line, line_no, onboard, fd, stack);
-	else if (f == pint)
+	if (f == pint)
 		if (!*stack)
 			pint_err(line, line_no, onboard, fd, stack);
 	if (f == pop)
