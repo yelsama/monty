4472223dcd08f0453d744fd4f489b599e032cd32
Yassein Eltayeb Mohamed Idris <ymohamed@lab1r4s13.42abudhabi.ae>
2023-05-25 19:57:41 +0400

    up

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ void	execute_line(char *line, int line_no, char *onboard,
 		tmp += 4;
 		while (*tmp == ' ')
 			tmp++;
-		if (*tmp1 == '\n' || !*tmp1)
+		if (*tmp == '\n' || !*tmp)
 			push_err(line, line_no, onboard, fd, stack);
 	}
 	if (f == pint)
